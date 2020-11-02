c1fe0931c826e1ae9282d56334354c0b4c3ea208
Erik <snowy@snowballs-MacBook-Pro.local>
2020-11-01 21:26:03 -0800

    Fix css layout

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-import { faGithub, faLinkedin, faFacebook, faTwitter} from '@fortawesome/free-brands-svg-icons'
-import { faCamera, faBars } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin, faFacebook, faTwitter, faInstagram} from '@fortawesome/free-brands-svg-icons'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function Home() {
@@ -23,7 +23,7 @@ export default function Home() {
             <a className={styles.topbarNavItem}><FontAwesomeIcon icon={faLinkedin} /></a>
             <a className={styles.topbarNavItem}><FontAwesomeIcon icon={faFacebook} /></a>
             <a className={styles.topbarNavItem}><FontAwesomeIcon icon={faTwitter} /></a>
-            <a className={styles.topbarNavItem}><FontAwesomeIcon icon={faCamera} /></a>
+            <a className={styles.topbarNavItem}><FontAwesomeIcon icon={faInstagram} /></a>
             <a className={styles.topbarNavBurger}><FontAwesomeIcon icon={faBars} /></a>
           </div>
         </div>
@@ -32,21 +32,38 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.mainContainer}>
           <div className={styles.margin}>
-            <div className={styles.marginContainer}>test</div>
+            <div className={styles.marginContainer}>
+              <ul className={styles.menu}>
+                <p>Posts</p>
+                <li><a href="default.asp">Home</a></li>
+                <li><a href="news.asp">News</a></li>
+                <li><a href="contact.asp">Contact</a></li>
+                <p>Misc</p>
+                <li><a href="about.asp">About</a></li>
+              </ul>
+            </div>
           </div>
           <div className={styles.content}>
-            <div className={styles.contentContainer}>test</div>
+            <div className={styles.contentContainer}>
+                <div className={styles.contentCard}>
+                  <div className={styles.contentCardHeader}>
+                    <a>Message1</a>
+                    <a>2020-12-01</a>
+                  </div>
+                  <div className={styles.contentCardContainer}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                  </div>
+                </div>
+            </div>
           </div>
           <div className={styles.margin}>
-            <div className={styles.marginContainer}>test</div>
+            <div className={styles.marginContainer}></div>
           </div>
         </div>
       </main>
 
       <footer className={styles.footer}>
-        <a>
-          footer
-        </a>
+        <a></a>
       </footer>
     </div>
   )
