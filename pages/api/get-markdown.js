5791ae7a7e67f89345867de7efc8fbbb34af3832
Erik <snowy@snowballs-MacBook-Pro.local>
2020-11-04 22:54:35 -0800

    Add lib dir for handlers

--- pages/api/get-markdown.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs'
-import { join } from 'path'
-import matter from 'gray-matter'
-
-const postsDirectory = '../../data';
-
-export function getPostSlugs() {
-  return fs.readdirSync(postsDirectory)
-}
