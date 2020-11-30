f0786a7ffef66f75fb65685155097405ec9abdba
Erik <snowy@snowballs-MacBook-Pro.local>
2020-11-29 21:51:47 -0800

    Add posts component

--- components/content.js
+++ components/content.js
@@ -1,13 +1,23 @@
 import Card from './card.js'
+import Post from './post.js'
 
 export default function Content({posts}) {
-
-    return (
-        <div class="flex-column justify-center">
-            {posts.map(post => 
-                <Card post={post}/>
-            )}
-        </div>
-    )
+    console.log('len', posts.length)
+    if (posts.length > 1) {
+        return (
+            <div class="flex-column justify-center">
+                {posts.map(post => 
+                    <Card post={post}/>
+                )}
+            </div>
+        )
+    } else {
+        return (
+            <div class="flex-column justify-center">
+                <Post post={posts[0]}/>
+            </div> 
+        )
+    }
     
+
 }
---
+++ components/post.js
@@ -0,0 +1,19 @@
+import ReactMarkdown from 'react-markdown'
+
+export default function Post(props) {
+    return (
+        <div class="w-full pb-12">
+            <div >
+
+                <div class="flex flex-column justify-center px-5 py-3 text-xl font-bold">
+                <a>{props.post.title}</a>
+                <a>{props.post.date}</a>
+                </div>
+                <div class="prose px-6 py-6">
+                    <ReactMarkdown source={props.post.content}></ReactMarkdown>
+                </div>
+
+            </div>
+        </div>
+    )
+}
