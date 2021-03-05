fb3221d700313c3dfa7d5d854157307909722b89
Erik <snowy@snowballs-MacBook-Pro.local>
2021-03-05 12:52:55 -0800

    Change marge on menu

--- components/taggedContent.js
+++ components/taggedContent.js
@@ -8,7 +8,7 @@ export default function TaggedContent({posts, tags}) {
         <div class="my-20 px-10 py-20 md:py-20 font-mono">
             <div class="grid grid-cols-5 gap-4">
                 <div class="col-start-1 col-end-2 invisible md:visible flex justify-start">
-                    <div class="fixed text-md md:text-lg text-right">
+                    <div class="fixed text-md md:text-lg text-right ml-7 mt-6">
                         <Menu tags={tags} flex={'flex flex-col text-menutxt'}/>
                     </div>
                 </div>
