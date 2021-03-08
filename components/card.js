18445f7b3d83853309091f1448f77409665cbbc2
Erik <snowy@snowballs-MacBook-Pro.local>
2021-03-08 00:39:50 -0800

    Feed card with share link and return with close

--- components/card.js
+++ components/card.js
@@ -1,15 +1,37 @@
-import ReactMarkdown from 'react-markdown'
+import { useRouter } from 'next/router';
+import ReactMarkdown from 'react-markdown';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function Card(props) {
+    const router = useRouter()
+    const feedMode = router.pathname === '/[tag]' ? 1 : 0;
+    console.log(props.tag)
     return (
         <div class="w-full pb-12">
             <div>
                 <div class="transition duration-500 ease-in-out shadow-card hover:shadow-cardhvr rounded ring-1 ring-cardbrd ring-offset-0 ring-offset-cardRingColor">
-                    <div class="flex flex-row justify-between px-5 py-3 bg-cardTopbg border-b-1 border-cardbrd text-xl">
+                    <div class="flex flex-row justify-between px-5 py-3 bg-cardTopbg border-b-1 border-cardbrd text-xl"
+                        onClick={() => {
+                            feedMode == 1 
+                            ? 
+                                router.push('/x/' + props.post.slug)
+                            : 
+                                router.push('/'+ props.post.tag)
+                            }
+                        }
+                    >
                         <a class="text-cardtxt cursor-default text-sm md:text-md lg:text-lg">{props.post.title}</a>
-                        <a class="text-cardtxt cursor-default text-xs md:text-sm lg:text-md">{props.post.date}</a>
+                        <a class="text-cardtxt cursor-default text-sm md:text-md lg:text-lg">
+                            {
+                                feedMode == 1 
+                                ? 
+                                    <FontAwesomeIcon icon={['fas', 'share']}></FontAwesomeIcon> 
+                                : 
+                                    <FontAwesomeIcon icon={['fas', 'times']}></FontAwesomeIcon>  
+                            }
+                        </a>
                     </div>
-                    <div class="bg-cardBodybg text-menutxt rounded-b prose-sm font-mono px-6 py-6 break-words rounded-b break-words">
+                    <div class="bg-cardBodybg text-menutxt rounded-b prose-sm font-mono px-6 py-6 break-words rounded-b">
                         <ReactMarkdown escapeHtml={false} source={props.post.content}></ReactMarkdown>
                     </div>
                 </div>
--- components/taggedContent.js
+++ components/taggedContent.js
@@ -4,6 +4,7 @@ import { useRouter } from 'next/router'
 
 export default function TaggedContent({posts, tags}) {
     const router = useRouter()
+    console.log('p', posts)
     return (
         <div class="my-20 px-10 py-20 md:py-20 font-mono">
             <div class="grid grid-cols-5 gap-4">
@@ -15,9 +16,9 @@ export default function TaggedContent({posts, tags}) {
                 <div class="col-start-1 col-end-6 md:col-start-2 md:col-end-6 lg:col-end-5">
                     <div class="flex-column justify-center">
                         {posts.map((post, index) => 
-                            <div onClick={() => {router.push('/x/' + post.slug).then(() => window.scrollTo(0, 0))}}>
+                            // <div onClick={() => {router.push('/x/' + post.slug).then(() => window.scrollTo(0, 0))}}>
                                 <Card key={index} post={post}/>
-                            </div>
+                            // </div>
                         )}
                     </div>
                 </div>
